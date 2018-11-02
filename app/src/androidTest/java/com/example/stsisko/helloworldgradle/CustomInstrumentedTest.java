a575e5396ae2f68ef00de97075888ac96c31b155
stsiskomichelis <tsiskost@gmail.com>
2018-11-02 12:10:23 +0200

    Repository clean - only new android app

---
+++ app/src/androidTest/java/com/example/stsisko/helloworldgradle/CustomInstrumentedTest.java
@@ -0,0 +1,39 @@
+package com.example.stsisko.helloworldgradle;
+
+
+import org.junit.runner.RunWith;
+
+import gr.aueb.android.barista.core.MyTestRunner;
+import gr.aueb.android.barista.core.annotations.SaySomething;
+
+//todo how to extend android test runnner
+@RunWith(MyTestRunner.class)
+public class CustomInstrumentedTest {
+
+    @SaySomething
+    int field1;
+
+    @SaySomething
+    String filed2;
+
+    @SaySomething
+    public void test1(){
+
+    }
+
+    @SaySomething
+    public void test2(){
+
+    }
+
+    @SaySomething
+    public void test4(){
+
+    }
+
+    @SaySomething(param1="test3",param2 = "test3.2")
+    public void test3(){
+
+    }
+
+}
